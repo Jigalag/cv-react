e6a534e71abfc7ee72cfe7b63cf085735d88603a
Veprev Alexey <funkylex@yandex.ru>
2018-11-27 17:05:18 +0200

    Updates

--- precache-manifest.874962f1671768778737f1f673c07796.js
+++ precache-manifest.874962f1671768778737f1f673c07796.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/cv-react/static/media/visbycf-regularoblique-webfont.84f50bcd.woff2"
   },
   {
-    "revision": "2db9909db9adb8287210",
+    "revision": "7469815038ea3ba2264d",
     "url": "/cv-react/static/css/main.5429f7e5.chunk.css"
   },
   {
@@ -20,68 +20,68 @@ self.__precacheManifest = [
     "url": "/cv-react/static/media/visbycf-demibold-webfont.8d4ed4fd.woff2"
   },
   {
-    "revision": "56ec440b9deae7a2ffcc4fafa105692f",
-    "url": "/cv-react/static/media/visbycf-bold-webfont.56ec440b.woff2"
-  },
-  {
     "revision": "55fa157e45f6c1f4e34654665ba2f85e",
     "url": "/cv-react/static/media/visbycf-extrabold-webfont.55fa157e.woff2"
+  },
+  {
+    "revision": "56ec440b9deae7a2ffcc4fafa105692f",
+    "url": "/cv-react/static/media/visbycf-bold-webfont.56ec440b.woff2"
   },
   {
     "revision": "c0b6df5e8ae1cc8e1fbe8efd2f6a64a2",
     "url": "/cv-react/static/media/visbycf-heavy-webfont.c0b6df5e.woff2"
   },
   {
-    "revision": "30c17d09379d149ebf89b1e82c3fc8bc",
-    "url": "/cv-react/static/media/visbycf-light-webfont.30c17d09.woff2"
-  },
-  {
     "revision": "95aa30211ee9e9c66c121eabe6020d55",
     "url": "/cv-react/static/media/visbycf-medium-webfont.95aa3021.woff2"
   },
   {
-    "revision": "b7f6fd6aa3c6a6b39cca473fee5d5342",
-    "url": "/cv-react/static/media/visbycf-regular-webfont.b7f6fd6a.woff2"
+    "revision": "30c17d09379d149ebf89b1e82c3fc8bc",
+    "url": "/cv-react/static/media/visbycf-light-webfont.30c17d09.woff2"
   },
   {
     "revision": "85f5588e6500bcbd42fca94eda5e3da4",
     "url": "/cv-react/static/media/visbycf-mediumoblique-webfont.85f5588e.woff2"
   },
   {
-    "revision": "2db9909db9adb8287210",
-    "url": "/cv-react/static/js/main.2db9909d.chunk.js"
+    "revision": "b7f6fd6aa3c6a6b39cca473fee5d5342",
+    "url": "/cv-react/static/media/visbycf-regular-webfont.b7f6fd6a.woff2"
+  },
+  {
+    "revision": "7469815038ea3ba2264d",
+    "url": "/cv-react/static/js/main.74698150.chunk.js"
   },
   {
     "revision": "878d7e4fa23da26a1500967e5905f71c",
     "url": "/cv-react/static/media/visbycf-bold-webfont.878d7e4f.woff"
   },
   {
-    "revision": "3fe3817acc806e5513270dfc7f26f130",
-    "url": "/cv-react/static/media/visbycf-extrabold-webfont.3fe3817a.woff"
+    "revision": "f5946495cd561ece00863b0c75492f31",
+    "url": "/cv-react/static/media/visbycf-demibold-webfont.f5946495.woff"
   },
   {
     "revision": "325e7902106fc0ba71423ec0685b0609",
     "url": "/cv-react/static/media/visbycf-heavy-webfont.325e7902.woff"
   },
   {
-    "revision": "f5946495cd561ece00863b0c75492f31",
-    "url": "/cv-react/static/media/visbycf-demibold-webfont.f5946495.woff"
-  },
-  {
-    "revision": "d54846720fe85589ef9774bec1839484",
-    "url": "/cv-react/static/media/visbycf-medium-webfont.d5484672.woff"
+    "revision": "3fe3817acc806e5513270dfc7f26f130",
+    "url": "/cv-react/static/media/visbycf-extrabold-webfont.3fe3817a.woff"
   },
   {
     "revision": "bf4629a2481051b0a66802a0eebfc1ba",
     "url": "/cv-react/static/media/visbycf-light-webfont.bf4629a2.woff"
   },
   {
-    "revision": "9a749fa7c51deaf2b25c3475ca54bfb4",
-    "url": "/cv-react/static/media/visbycf-mediumoblique-webfont.9a749fa7.woff"
+    "revision": "d54846720fe85589ef9774bec1839484",
+    "url": "/cv-react/static/media/visbycf-medium-webfont.d5484672.woff"
   },
   {
     "revision": "b34b3d194a34f37c8d2036d193f2e5e2",
     "url": "/cv-react/static/media/visbycf-regular-webfont.b34b3d19.woff"
+  },
+  {
+    "revision": "9a749fa7c51deaf2b25c3475ca54bfb4",
+    "url": "/cv-react/static/media/visbycf-mediumoblique-webfont.9a749fa7.woff"
   },
   {
     "revision": "109e806e300aa8635ef52eb9c94561f4",
@@ -96,7 +96,7 @@ self.__precacheManifest = [
     "url": "/cv-react/static/media/preview.dcfe0e19.jpg"
   },
   {
-    "revision": "792b5ab4943cb2b1e88dfc787deca366",
+    "revision": "5352012983eb74dc482344a1e5d93b45",
     "url": "/cv-react/index.html"
   }
 ];
