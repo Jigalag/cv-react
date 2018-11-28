0906e130ba0eae9fcd826e3d9bda7f8ce9ebdebc
Veprev Alexey <funkylex@yandex.ru>
2018-11-28 16:21:26 +0200

    contact page

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react';
+import contactData from '../../data/contact'
 
 class Contact extends Component {
     render() {
+        const contactItem = contactData.social_links.map(link =>
+            <li className="skill">
+                <a href={link.url}>
+                    <span dangerouslySetInnerHTML={{__html: link.ico}}>
+                    </span>
+                    {link.title}
+                </a>
+            </li>
+        );
         return (
-            <div>5</div>
+            <div>
+                <div className="container smallContainer">
+                    <div className="simpleText textCenter contactPage">
+                        <h2>Contacts</h2>
+                        <p>{ contactData.contact_text }</p>
+                    </div>
+                </div>
+                <div className="purpleSection">
+                    <div className="container">
+                        <div className="workItem contactItem">
+                            <div className="simpleText textLeft floatLeft fullWidth">
+                                <ul className="contactsList">
+                                    {contactItem}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
