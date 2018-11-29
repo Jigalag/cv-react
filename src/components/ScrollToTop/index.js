066ac940084fc3d1639f5bfd3602b6b3e850966b
Veprev Alexey <funkylex@yandex.ru>
2018-11-29 10:32:14 +0200

    scroll to top component

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -1,5 +1,6 @@
 import {Component} from 'react';
-import {withRouter} from "react-router-dom"
+import {withRouter} from "react-router-dom";
+
 class ScrollToTop extends Component {
     componentDidUpdate(prevProps) {
         if (this.props.location !== prevProps.location) {
