47f9fa4167112ff7a0a3a8b132e956247d1193b5
Veprev Alexey <funkylex@yandex.ru>
2018-12-21 12:19:34 +0200

    fix date of work and web site for companies

--- src/app/components/CompanyPage/companyItem.js
+++ src/app/components/CompanyPage/companyItem.js
@@ -6,20 +6,20 @@ function CompanyItem(props) {
     const technologies = company.technologies.map((tech, key) =>
         <span key={key}>{tech}{key < (company.technologies.length - 1) ? ',' : ''}</span>
     );
-    const webSite = company.webSite && (
+    const webSite = company['webSite'] && (
         <li className="skill">
             <span>Website</span>
             <span>
-                <a href={company.webSite} target="_blank" rel="noopener noreferrer">
-                    {company.webSite}
+                <a href={company['webSite']} target="_blank" rel="noopener noreferrer">
+                    {company['webSite']}
                 </a>
             </span>
         </li>
     );
-    const startDate = company.startDate && (
+    const startDate = company['startDate'] && (
         <li className="skill">
             <span>Worked</span>
-            <span>{company.startDate} - {company.endDate ? company.endDate : 'Now'}</span>
+            <span>{company['startDate']} - {company['endDate'] ? company['endDate'] : 'Now'}</span>
         </li>
     );
     return (
